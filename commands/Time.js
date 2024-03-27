c804238d4f3ae42e716e4a0a9bf1a0eddbb381ca
꧁Voltage꧂ <f2v74yt@gmail.com>
2024-03-27 09:48:28 -0500

    Update Time.js

--- commands/Time.js
+++ commands/Time.js
@@ -3,21 +3,28 @@
 module.exports = {
     data: {
         name: "Time",
-        description: "Displays the current time",
+        description: "Displays the current date, time, day of the week, and time zone",
         usage: "time",
-        author: "Your Name",
+        author: "AkiEa",
         info: {
-            version: "1.0",
+            version: "2.0",
             stable: true,
             experimental: false
         },
         action: function() {
             // Get the current date and time
             const currentTime = new Date();
+            const formattedDate = currentTime.toLocaleDateString(undefined, {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
             const formattedTime = currentTime.toLocaleTimeString();
+            const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-            // Display the current time
-            return ('Current time:', formattedTime);
+            // Display the current date, time, day of the week, and time zone
+            return `Current date: ${formattedDate}\nCurrent time: ${formattedTime}\nDay of the week: ${formattedDate.split(',')[0]}\nTime zone: ${timeZone}`;
         }
     }
 }
